872a26eb467cb9e62a07f0410da8b4dbda8a4153
KarmaNamgay <132356064+KarmaNamgay@users.noreply.github.com>
2024-01-05 09:04:59 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function intro_page()
+{
+    window.location.href = "index.html";
+}
+
+function causes_page()
+{
+    window.location.href = "causes.html";
+}
+
+function impact_page()
+{
+    window.location.href = "impact.html";
+}
+
+function conclusion_page()
+{
+    window.location.href = "conclusions.html";
+}
